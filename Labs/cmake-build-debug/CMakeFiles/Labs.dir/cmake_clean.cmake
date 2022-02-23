705b7f669d757184f597f89c79997ecc821bc97e
Siko Szabolcs <sikoszabolcs17@yahoo.com>
2022-02-23 12:56:40 +0200

    Inital commit

---
+++ Labs/cmake-build-debug/CMakeFiles/Labs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labs.dir/main.c.obj"
+  "Labs.exe"
+  "Labs.exe.manifest"
+  "Labs.lib"
+  "Labs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Labs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
