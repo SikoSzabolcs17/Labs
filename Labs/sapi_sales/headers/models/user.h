af65d1c078d4f51711b34dd780e59553865b2cb3
Siko Szabolcs <sikoszabolcs17@yahoo.com>
2022-03-01 23:20:45 +0200

    2.0

---
+++ Labs/sapi_sales/headers/models/user.h
@@ -0,0 +1,13 @@
+//
+// Created by Siko Szabolcs on 2022. 02. 23..
+//
+
+#ifndef LABS_USER_H
+#define LABS_USER_H
+
+typedef struct{
+    int Id;
+    char* Name;
+}User;
+
+#endif //LABS_USER_H
---
+++ Labs/shared/headers/useful.h
@@ -0,0 +1,8 @@
+//
+// Created by Siko Szabolcs on 2022. 02. 23..
+//
+
+#ifndef LABS_USEFUL_H
+#define LABS_USEFUL_H
+
+#endif //LABS_USEFUL_H
